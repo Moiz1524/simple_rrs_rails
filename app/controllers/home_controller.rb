02ab25f25d54b42f810a947915d9404589887591
Imran Hayat <imran.hayat92@gmail.com>
2019-04-09 13:27:47 +0000

    Final tweaks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
     before_action :authenticate_user!
     
     def index
-       @tickets = Ticket.where(current_user.id) 
+       @tickets = Ticket.where(:user_id => current_user.id) 
     end
 end
